307c37ed496c1a0217db8ec94f8d504af7f96708
Denas <56196733+Ddog69@users.noreply.github.com>
2019-10-23 10:20:41 +0100

    Update LoginScreen.php

--- templates/LoginScreen.php
+++ templates/LoginScreen.php
@@ -18,7 +18,7 @@
     <input  type="hidden" name="action" value="processLogin">
 
     <label for = "username"> Enter Username:</label>
-    <input type="text" name = "username" id = "username" placeholder = "Username" required>
+    <input type="text" name = "username" id = "username" placeholder = "Username" minlength="5" maxlength="20" required>
     <br>
     <label for = "password"> Enter Password:</label>
     <input type="password" name = "password" id ="password" placeholder = "Password"  required>
